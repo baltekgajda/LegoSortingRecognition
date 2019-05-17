ca431a506cfd4571404386819bcbcfba0d0a2c77
Martyna Kania <martynaakaniaa@gmail.com>
2019-05-17 18:46:33 +0200

    Split data into 3 subsets, add some random transformations for data augmentation.

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -1,25 +1,51 @@
-from __future__ import print_function, division
-
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 from torchvision.datasets import ImageFolder
 from torchvision.utils import make_grid
 
 
-def load_data() -> DataLoader:
-    data_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
+def load_data(train_size=0.7, valid_size=0.2, test_size=0.1) \
+        -> (DataLoader, DataLoader, DataLoader):
     dataset = ImageFolder(root='data\\Base Images\\',
-                          transform=data_transform)
-    data_loader = DataLoader(dataset,
-                             batch_size=4, shuffle=True,
-                             num_workers=4)
-    return data_loader
+                          transform=transforms.Compose([
+                              transforms.RandomResizedCrop(224, scale=(0.85, 1)),
+                              transforms.RandomVerticalFlip(),
+                              transforms.RandomHorizontalFlip(),
+                              transforms.ToTensor(),
+                              transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                   std=[0.229, 0.224, 0.225]),
+                          ]))
+
+    train_indices, valid_indices, test_indices = (
+        split_indices(len(dataset), train_size, valid_size, test_size)
+    )
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=4,
+                                               sampler=SubsetRandomSampler(train_indices))
+    valid_loader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=4,
+                                               sampler=SubsetRandomSampler(valid_indices))
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=4,
+                                              sampler=SubsetRandomSampler(test_indices))
+    return train_loader, valid_loader, test_loader
+
+
+def split_indices(dataset_len, train_size, valid_size, test_size) \
+        -> (torch.Tensor, torch.Tensor, torch.Tensor):
+    if train_size + valid_size + test_size > 1:
+        raise ValueError('Sum of train_size, valid_size, test_size must be lower than 1')
+
+    indices = torch.randperm(dataset_len)
+
+    train_indices = indices[:int(len(indices) * train_size)]
+    idx = int(len(indices) * train_size)
+    valid_indices = indices[idx: int(len(indices) * valid_size) + idx]
+    idx += int(len(indices) * valid_size)
+    test_indices = indices[idx: int(len(indices) * test_size) + idx]
+
+    return train_indices, valid_indices, test_indices
 
 
 def show_random_images(data_loader: DataLoader):
--- main_script.py
+++ main_script.py
@@ -4,5 +4,5 @@ from data_loader import load_data, show_random_images
 
 if __name__ == '__main__':
     freeze_support()
-    data_loader = load_data()
-    show_random_images(data_loader)
+    train_loader, valid_loader, test_loader = load_data()
+    show_random_images(train_loader)
