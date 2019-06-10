196924bbc0a7065a1d00cd314409a44937e50b86
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-06-10 21:20:10 +0200

    Add argument to save CMC plot in given location

--- net_test_and_metrics.py
+++ net_test_and_metrics.py
@@ -4,11 +4,10 @@ import torch
 import evaluation_metrics
 
 
-def test_network(net, test_data_loader, device, plot_name='plot'):
+def test_network(net, test_data_loader, device, plots_folder='./results/', plot_name='plot'):
     """Test the network on the test data and calculate metrics
 
     :param net: trained neural network
-    :param test_data_loader: data loader with test data
     :param test_data_loader: data loader with test data
     :return: dict with accuracy, top 1 error and top 5 error
     """
@@ -29,9 +28,10 @@ def test_network(net, test_data_loader, device, plot_name='plot'):
         'top_5_error': evaluation_metrics.top_n_error(true_classes, probabilities, n=5)
     }
     cumulative_ranks = evaluation_metrics.get_cumulative_ranks(true_classes, probabilities)
+    filename = plots_folder + 'cmc_{}_{}'.format(plot_name, datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
     evaluation_metrics.plot_cmc(
         cumulative_ranks,
-        filename='./results/cmc_{}_{}'.format(plot_name, datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+        filename=filename
     )
 
     return metrics
