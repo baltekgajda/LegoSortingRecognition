f3006c1ca38dc940f6f15101e2116f6dc36d6b76
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-06-16 15:43:34 +0200

    Change learning rate through param

--- feature_extraction.py
+++ feature_extraction.py
@@ -88,30 +88,30 @@ def get_params_to_update(model):
     return params_to_update
 
 
-def train_classifier_only(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10):
-    return _train_model_for_scenario(1, "vgg-classifier-only", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs)
+def train_classifier_only(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10, lr=0.001):
+    return _train_model_for_scenario(1, "vgg-classifier-only", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs, lr=lr)
 
 
-def train_classifier_and_last_conv(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10):
-    return _train_model_for_scenario(2, "vgg-last_conv", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs)
+def train_classifier_and_last_conv(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10, lr=0.001):
+    return _train_model_for_scenario(2, "vgg-last_conv", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs, lr=lr)
 
 
-def train_full_net(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10):
-    return _train_model_for_scenario(3, "vgg-full-net", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs)
+def train_full_net(dataloaders_dicts, models_folder, device, num_of_classes=20, num_of_epochs=10, lr=0.001):
+    return _train_model_for_scenario(3, "vgg-full-net", models_folder, dataloaders_dicts, device, num_of_classes, num_of_epochs, lr=lr)
 
 
-def train_simplified_net(model, dataloaders_dicts, models_folder, device, num_of_epochs=10):
+def train_simplified_net(model, dataloaders_dicts, models_folder, device, num_of_epochs=10, lr=0.001):
     simpler_model = VGGFactory.simplify_model(model)
-    optimizer = optim.SGD(get_params_to_update(model), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(get_params_to_update(model), lr=lr, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
     model_ft, val_hist, train_hist = train_model(simpler_model, dataloaders_dicts, criterion, optimizer, device, num_epochs=num_of_epochs)
     torch.save(model_ft, models_folder + "vgg-simplified" + ".pth")
     return model_ft, val_hist, train_hist
 
 
-def _train_model_for_scenario(scenario_id, model_name, models_folder,  dataloaders_dicts, device, num_of_classes=20, num_of_epochs=10):
+def _train_model_for_scenario(scenario_id, model_name, models_folder,  dataloaders_dicts, device, num_of_classes=20, num_of_epochs=10, lr=0.001):
     model = VGGFactory.create_model(scenario_id, num_of_classes)
-    optimizer = optim.SGD(get_params_to_update(model), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(get_params_to_update(model), lr=lr, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
     model_ft, val_hist, train_hist = train_model(model, dataloaders_dicts, criterion, optimizer, device, num_epochs=num_of_epochs)
     torch.save(model_ft, models_folder + model_name + ".pth")
